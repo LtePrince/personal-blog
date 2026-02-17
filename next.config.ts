6c3ca075dff30dfadd394e3ea02bcd9e02ccf213
Adolph <574699248@qq.com>
2026-02-17 21:57:17 +1100

    static resource url rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    const backendUrl = process.env.BLOG_BACKEND_URL || "http://localhost:8080";
+    return [
+      {
+        source: "/static/:path*",
+        destination: `${backendUrl}/static/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
